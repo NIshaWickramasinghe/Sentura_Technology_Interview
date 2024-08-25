d00523bb69084a799a43e9ec8efcf20a4d75a023
NIshaWickramasinghe <nishawickramasinghe73@gmail.com>
2024-08-25 09:21:44 +0530

    Initialized project

---
+++ src/main/java/lk/ijse/weavy_api/WeavyApiApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.weavy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavyApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WeavyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/weavy_api/WeavyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.weavy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
